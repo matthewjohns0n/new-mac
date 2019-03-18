3f28d19a4571bac74450472c5e343b9e1f49577c
Matt Johnson <matt@packettide.com>
2019-03-18 18:06:45 -0400

    Add comments

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 # install brew:
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Update brew
 brew update
 
+# Install brew bundle
 brew tap Homebrew/bundle
