f4d142ed520ac29b11e6254c28b3f1bc8d42e84c
Matt Johnson <matt@packettide.com>
2019-03-19 15:12:14 -0400

    Split install to personal and work

---
+++ install-personal.sh
@@ -0,0 +1,5 @@
+
+brew bundle --file='Brewfile-personal' install
+
+# Open Creative cloud installer app for lightroom
+open -a /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
--- install.sh
+++ install.sh
@@ -10,6 +10,3 @@ brew tap Homebrew/bundle
 
 # Install files from brewfile bundle
 brew bundle install
-
-# Open Creative cloud installer app for lightroom
-open -a /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
