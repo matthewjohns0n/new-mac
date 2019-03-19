e1b89819b6c351b20146103a6e4aca836d5b8e36
Matt Johnson <matt@packettide.com>
2019-03-19 14:41:21 -0400

    Brewfile, valet install...

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ brew update
 
 # Install brew bundle
 brew tap Homebrew/bundle
+
+# Install files from brewfile bundle
+brew bundle install
+
+# Open Creative cloud installer app for lightroom
+open -a /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
---
+++ valet.sh
@@ -0,0 +1,4 @@
+brew update
+
+brew install php
+brew install composer
