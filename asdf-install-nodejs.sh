1dd9bcae2c1a71e2b526921cf4f2e3a2226544eb
Matt Johnson <matt@packettide.com>
2019-04-30 13:34:12 -0400

    Added asdf install commands for managing node versions

---
+++ asdf-install-nodejs.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.1
+asdf plugin-add nodejs
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf install nodejs 11.14.0
+asdf global nodejs 11.14.0
