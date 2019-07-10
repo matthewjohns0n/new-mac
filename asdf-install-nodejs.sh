355f115d689b157da9ea5925fb5923820502b34b
Matt Johnson <matt@packettide.com>
2019-07-10 15:49:15 -0400

    Install gulp

--- asdf-install-nodejs.sh
+++ asdf-install-nodejs.sh
@@ -3,3 +3,6 @@ asdf plugin-add nodejs
 bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 asdf install nodejs 11.14.0
 asdf global nodejs 11.14.0
+
+# Install gulp
+npm install --global gulp-cli
